b93d5d946734065e753256015bb9c0c394be8bba
Blueming <1693145038@qq.com>
2023-08-20 23:40:17 +0800

    update: readme

--- src/views/home/components/home-channel.vue
+++ src/views/home/components/home-channel.vue
@@ -1,16 +1,32 @@
 <template>
     <van-tabs v-model:active="active">
-        <van-tab title="标签 1">内容 1</van-tab>
-        <van-tab title="标签 2">内容 2</van-tab>
-        <van-tab title="标签 3">内容 3</van-tab>
-        <van-tab title="标签 4">内容 4</van-tab>
+        <van-tab v-for="item in list" :key="item.id" :title="item.text"></van-tab>
     </van-tabs>
 </template>
     
 <script setup lang='ts'>
+
 import { ref } from "vue";
+import axios from "axios"
+
+//ts的接口用于标记数据格式
+interface INavItem {
+    id: string
+    text: string
+}
 
 const active = ref(0)
+//频道数据
+const list = ref<INavItem[]>([])
+
+
+axios({
+    url: "/navList",
+    method: "get",
+}).then(res => {
+    console.log(res.data);
+    list.value = res.data.result
+})
 </script>
     
 <style></style>
